383d67d5dc195439f538251fb0ddaee7ddb952b8
Christopher Hardin <47571165+chrishardin@users.noreply.github.com>
2020-03-04 02:03:53 -0600

    Update global_rules.py

--- global_rules.py
+++ global_rules.py
@@ -6,7 +6,7 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes = 1)
 def timed_job():
     print('This job is running every minute')
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=2, minute=5):
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=2, minute=5)
 def scheduled_job():
     print('this job is running every day at 1:32')
 
