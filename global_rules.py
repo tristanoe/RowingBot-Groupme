2dba3426fb8c731adc603188854490160e088106
Christopher Hardin <47571165+chrishardin@users.noreply.github.com>
2020-03-04 02:06:05 -0600

    Update global_rules.py

--- global_rules.py
+++ global_rules.py
@@ -1,16 +1,6 @@
 import urllib.request, json
 from decimal import Decimal
-from apscheduler.schedulers.blocking import BlockingScheduler
 
-sched = BlockingScheduler()
-@sched.scheduled_job('interval', minutes = 1)
-def timed_job():
-    print('This job is running every minute')
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=2, minute=5)
-def scheduled_job():
-    print('this job is running every day at 1:32')
-
-sched.start()
  
 
 def run(data, bot_info, send):
