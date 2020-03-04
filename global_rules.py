4e8c59bcb5fac4222aff4078ba002e10dc4dadce
Christopher Hardin <47571165+chrishardin@users.noreply.github.com>
2020-03-04 00:57:59 -0600

    Update global_rules.py

--- global_rules.py
+++ global_rules.py
@@ -49,8 +49,8 @@ def getTemp():
             
     with urllib.request.urlopen("https://openweathermap.org/data/2.5/weather?lat=38.9806&lon=-95.2429&appid=b6907d289e10d714a6e88b30761fae22&units=imperial") as url2:
         data_temp2 = json.loads(url2.read().decode())
-        air_temp = data_temp2["main"][0]["temp"]
-        wind = data_temp2["wind"][0]["speed"]
+        air_temp = str(data_temp2["main"]["temp"]) + " F"
+        wind = str(data_temp2["wind"]["speed"]) + " mph"
     sentence_wind = "\n"+str(air_temp) + " " + str(wind)
     return sentence_temp + sentence_wind
         
