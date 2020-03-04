8ed71bf6adea87e555edf4946213762ac2bc2f5e
Christopher Hardin <47571165+chrishardin@users.noreply.github.com>
2020-03-04 01:37:06 -0600

    Update clock.py

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes = 1)
 def timed_job():
     print('This job is running every minute')
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=1, minute=32):
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=1, minute=40):
 def scheduled_job():
     print('this job is running every day at 1:32')
 
