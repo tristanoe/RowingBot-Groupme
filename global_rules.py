d62e8bbfd5ddad61b98ff8e3b12ea2503815e1cd
Christopher Hardin <47571165+chrishardin@users.noreply.github.com>
2020-03-06 02:35:51 -0600

    Update global_rules.py

--- global_rules.py
+++ global_rules.py
@@ -27,7 +27,7 @@ def run(data, bot_info, send):
             send("Is Zach making up commands again? smh", bot_info[0])
             return True
         else:
-            send("{} isn't a command, sorry {}.".format(data['text'], data['name']), bot_info[0])
+            send("{} isn't a command, sorry {}".format(data['text'], data['name']), bot_info[0])
             return True
    # send(message[1:], bot_info[0])
    # send(message[:1], bot_info[0])
