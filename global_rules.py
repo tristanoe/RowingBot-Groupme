4f62edee6799298317d9f3e1a9477650034f7b6d
Christopher Hardin <47571165+chrishardin@users.noreply.github.com>
2020-03-06 01:00:59 -0600

    Update global_rules.py

--- global_rules.py
+++ global_rules.py
@@ -23,9 +23,14 @@ def run(data, bot_info, send):
 
         send("http://snowfence.umn.edu/Images/Wind/wind_bln1.gif", bot_info[0])
         return True
-        
-    #send(clock.timed_job(), bot_info[0])
-    #return True
+      
+    if message[1:] == '.':
+        if data['name'] == 'Zach Hardin':
+            send("Is Zach making up commands again?", bot_info[0]))
+            return True
+        else:
+            send("{} isn't a command, sorry {}.".format(data['text'], data['name']), bot_info[0])
+            return True
 
 def getFlow():
    # global flow_lcn, flow_lfk
