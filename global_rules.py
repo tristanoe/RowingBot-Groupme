583cad288eb5345b12ca370eab1e84b2769a776c
tristanoe <89527490+tristanoe@users.noreply.github.com>
2022-03-07 20:13:37 -0600

    Update global_rules.py

--- global_rules.py
+++ global_rules.py
@@ -5,7 +5,7 @@ from decimal import Decimal
  
 
 def run(data, bot_info, send):
-    help_message = "\n\n.flow -> Rowing info\n.exec -> Exec Board\n.compass -> Compass\n.8ball -> say 8ball followed by a message"
+    help_message = "\n\n.flow -> Rowing info\n.exec -> Exec Board\n.compass -> Compass\n.8ball -> say 8ball followed by a message\nMade by: Chris Hardin\nHosted by: Tristan Oestreich"
     exec_message = "Exec Board Spring 2022\n------------------\nPresident: Grant Hamilton\nVice President: Tristan Oestreich\nMens Captain: Noah Johnson\nWomens Captain: Svenja Horn\nCoxswain Captain: None\nTresurer: Will Doerries\nSecretary: Josh Ryan \nPR/Alumni Rep: Landyn Welch\nRecruitment Chair: Jude Butler\n\n"
     zach_message = "."
     lol = ['NOAHAHHA WHYY', 'seriously noah?', 'come on man.. weve been over this.. smh..', 'are you still trying?', 'try again. I dare you.', 'someone come get there mans', 'NOAAHAHA', 'Mr. Johnson, thank you for trying a command that currently doesnt exist. Maybe sometime in the near future we will have it implemented. In the meantime, get back on the erg.', 'welp.',]
